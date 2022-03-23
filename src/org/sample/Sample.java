681e99ae19c1bff3b4534c8b7fbe2465af095329
sashwinsrinivas <ashwinsrinivas279@gmail.com>
2022-03-23 14:28:37 +0530

    Done by Ashwin

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -2,7 +2,15 @@ package org.sample;
 
 public class Sample {
 	
+	private void ashwin() {
+		
+		System.out.println("This code done by Ashwin");
+
+	}
+	
 	public static void main(String[] args) {
+		
+		
 		
 		System.out.println("Done pushing");
 	}
